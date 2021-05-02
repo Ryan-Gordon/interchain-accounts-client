cd43e98d04faee5b8769ba831f200de238791d76
Ryan Gordon <ryangordon210@gmail.com>
2021-05-02 17:58:34 +0100

    added codecs

---
+++ src/codec/intertx/tx.ts
@@ -0,0 +1,473 @@
+/* eslint-disable */
+import Long from "long";
+import _m0 from "protobufjs/minimal";
+import { Coin } from "../cosmos/base/v1beta1/coin";
+
+export const protobufPackage = "intertx";
+
+/** MsgRegisterAccount is used to register an interchain account on a target chain */
+export interface MsgRegisterAccount {
+  /** the port by which the packet will be sent */
+  sourcePort: string;
+  /** the channel by which the packet will be sent */
+  sourceChannel: string;
+  owner: string;
+}
+
+export interface MsgRegisterAccountResponse {}
+
+/** MsgSend is used to send coins from an interchain account to another account on the same chain */
+export interface MsgSend {
+  /** the port by which the packet will be sent */
+  chainType: string;
+  sourcePort: string;
+  /** the channel by which the packet will be sent */
+  sourceChannel: string;
+  sender: Uint8Array;
+  toAddress: Uint8Array;
+  amount: Coin[];
+}
+
+/** MsgSendResponse defines the Msg/Send response type. */
+export interface MsgSendResponse {}
+
+const baseMsgRegisterAccount: object = {
+  sourcePort: "",
+  sourceChannel: "",
+  owner: "",
+};
+
+export const MsgRegisterAccount = {
+  encode(
+    message: MsgRegisterAccount,
+    writer: _m0.Writer = _m0.Writer.create()
+  ): _m0.Writer {
+    if (message.sourcePort !== "") {
+      writer.uint32(18).string(message.sourcePort);
+    }
+    if (message.sourceChannel !== "") {
+      writer.uint32(26).string(message.sourceChannel);
+    }
+    if (message.owner !== "") {
+      writer.uint32(50).string(message.owner);
+    }
+    return writer;
+  },
+
+  decode(input: _m0.Reader | Uint8Array, length?: number): MsgRegisterAccount {
+    const reader = input instanceof _m0.Reader ? input : new _m0.Reader(input);
+    let end = length === undefined ? reader.len : reader.pos + length;
+    const message = { ...baseMsgRegisterAccount } as MsgRegisterAccount;
+    while (reader.pos < end) {
+      const tag = reader.uint32();
+      switch (tag >>> 3) {
+        case 2:
+          message.sourcePort = reader.string();
+          break;
+        case 3:
+          message.sourceChannel = reader.string();
+          break;
+        case 6:
+          message.owner = reader.string();
+          break;
+        default:
+          reader.skipType(tag & 7);
+          break;
+      }
+    }
+    return message;
+  },
+
+  fromJSON(object: any): MsgRegisterAccount {
+    const message = { ...baseMsgRegisterAccount } as MsgRegisterAccount;
+    if (object.sourcePort !== undefined && object.sourcePort !== null) {
+      message.sourcePort = String(object.sourcePort);
+    } else {
+      message.sourcePort = "";
+    }
+    if (object.sourceChannel !== undefined && object.sourceChannel !== null) {
+      message.sourceChannel = String(object.sourceChannel);
+    } else {
+      message.sourceChannel = "";
+    }
+    if (object.owner !== undefined && object.owner !== null) {
+      message.owner = String(object.owner);
+    } else {
+      message.owner = "";
+    }
+    return message;
+  },
+
+  toJSON(message: MsgRegisterAccount): unknown {
+    const obj: any = {};
+    message.sourcePort !== undefined && (obj.sourcePort = message.sourcePort);
+    message.sourceChannel !== undefined &&
+      (obj.sourceChannel = message.sourceChannel);
+    message.owner !== undefined && (obj.owner = message.owner);
+    return obj;
+  },
+
+  fromPartial(object: DeepPartial<MsgRegisterAccount>): MsgRegisterAccount {
+    const message = { ...baseMsgRegisterAccount } as MsgRegisterAccount;
+    if (object.sourcePort !== undefined && object.sourcePort !== null) {
+      message.sourcePort = object.sourcePort;
+    } else {
+      message.sourcePort = "";
+    }
+    if (object.sourceChannel !== undefined && object.sourceChannel !== null) {
+      message.sourceChannel = object.sourceChannel;
+    } else {
+      message.sourceChannel = "";
+    }
+    if (object.owner !== undefined && object.owner !== null) {
+      message.owner = object.owner;
+    } else {
+      message.owner = "";
+    }
+    return message;
+  },
+};
+
+const baseMsgRegisterAccountResponse: object = {};
+
+export const MsgRegisterAccountResponse = {
+  encode(
+    _: MsgRegisterAccountResponse,
+    writer: _m0.Writer = _m0.Writer.create()
+  ): _m0.Writer {
+    return writer;
+  },
+
+  decode(
+    input: _m0.Reader | Uint8Array,
+    length?: number
+  ): MsgRegisterAccountResponse {
+    const reader = input instanceof _m0.Reader ? input : new _m0.Reader(input);
+    let end = length === undefined ? reader.len : reader.pos + length;
+    const message = {
+      ...baseMsgRegisterAccountResponse,
+    } as MsgRegisterAccountResponse;
+    while (reader.pos < end) {
+      const tag = reader.uint32();
+      switch (tag >>> 3) {
+        default:
+          reader.skipType(tag & 7);
+          break;
+      }
+    }
+    return message;
+  },
+
+  fromJSON(_: any): MsgRegisterAccountResponse {
+    const message = {
+      ...baseMsgRegisterAccountResponse,
+    } as MsgRegisterAccountResponse;
+    return message;
+  },
+
+  toJSON(_: MsgRegisterAccountResponse): unknown {
+    const obj: any = {};
+    return obj;
+  },
+
+  fromPartial(
+    _: DeepPartial<MsgRegisterAccountResponse>
+  ): MsgRegisterAccountResponse {
+    const message = {
+      ...baseMsgRegisterAccountResponse,
+    } as MsgRegisterAccountResponse;
+    return message;
+  },
+};
+
+const baseMsgSend: object = {
+  chainType: "",
+  sourcePort: "",
+  sourceChannel: "",
+};
+
+export const MsgSend = {
+  encode(
+    message: MsgSend,
+    writer: _m0.Writer = _m0.Writer.create()
+  ): _m0.Writer {
+    if (message.chainType !== "") {
+      writer.uint32(10).string(message.chainType);
+    }
+    if (message.sourcePort !== "") {
+      writer.uint32(18).string(message.sourcePort);
+    }
+    if (message.sourceChannel !== "") {
+      writer.uint32(26).string(message.sourceChannel);
+    }
+    if (message.sender.length !== 0) {
+      writer.uint32(50).bytes(message.sender);
+    }
+    if (message.toAddress.length !== 0) {
+      writer.uint32(58).bytes(message.toAddress);
+    }
+    for (const v of message.amount) {
+      Coin.encode(v!, writer.uint32(66).fork()).ldelim();
+    }
+    return writer;
+  },
+
+  decode(input: _m0.Reader | Uint8Array, length?: number): MsgSend {
+    const reader = input instanceof _m0.Reader ? input : new _m0.Reader(input);
+    let end = length === undefined ? reader.len : reader.pos + length;
+    const message = { ...baseMsgSend } as MsgSend;
+    message.amount = [];
+    message.sender = new Uint8Array();
+    message.toAddress = new Uint8Array();
+    while (reader.pos < end) {
+      const tag = reader.uint32();
+      switch (tag >>> 3) {
+        case 1:
+          message.chainType = reader.string();
+          break;
+        case 2:
+          message.sourcePort = reader.string();
+          break;
+        case 3:
+          message.sourceChannel = reader.string();
+          break;
+        case 6:
+          message.sender = reader.bytes();
+          break;
+        case 7:
+          message.toAddress = reader.bytes();
+          break;
+        case 8:
+          message.amount.push(Coin.decode(reader, reader.uint32()));
+          break;
+        default:
+          reader.skipType(tag & 7);
+          break;
+      }
+    }
+    return message;
+  },
+
+  fromJSON(object: any): MsgSend {
+    const message = { ...baseMsgSend } as MsgSend;
+    message.amount = [];
+    message.sender = new Uint8Array();
+    message.toAddress = new Uint8Array();
+    if (object.chainType !== undefined && object.chainType !== null) {
+      message.chainType = String(object.chainType);
+    } else {
+      message.chainType = "";
+    }
+    if (object.sourcePort !== undefined && object.sourcePort !== null) {
+      message.sourcePort = String(object.sourcePort);
+    } else {
+      message.sourcePort = "";
+    }
+    if (object.sourceChannel !== undefined && object.sourceChannel !== null) {
+      message.sourceChannel = String(object.sourceChannel);
+    } else {
+      message.sourceChannel = "";
+    }
+    if (object.sender !== undefined && object.sender !== null) {
+      message.sender = bytesFromBase64(object.sender);
+    }
+    if (object.toAddress !== undefined && object.toAddress !== null) {
+      message.toAddress = bytesFromBase64(object.toAddress);
+    }
+    if (object.amount !== undefined && object.amount !== null) {
+      for (const e of object.amount) {
+        message.amount.push(Coin.fromJSON(e));
+      }
+    }
+    return message;
+  },
+
+  toJSON(message: MsgSend): unknown {
+    const obj: any = {};
+    message.chainType !== undefined && (obj.chainType = message.chainType);
+    message.sourcePort !== undefined && (obj.sourcePort = message.sourcePort);
+    message.sourceChannel !== undefined &&
+      (obj.sourceChannel = message.sourceChannel);
+    message.sender !== undefined &&
+      (obj.sender = base64FromBytes(
+        message.sender !== undefined ? message.sender : new Uint8Array()
+      ));
+    message.toAddress !== undefined &&
+      (obj.toAddress = base64FromBytes(
+        message.toAddress !== undefined ? message.toAddress : new Uint8Array()
+      ));
+    if (message.amount) {
+      obj.amount = message.amount.map((e) => (e ? Coin.toJSON(e) : undefined));
+    } else {
+      obj.amount = [];
+    }
+    return obj;
+  },
+
+  fromPartial(object: DeepPartial<MsgSend>): MsgSend {
+    const message = { ...baseMsgSend } as MsgSend;
+    message.amount = [];
+    if (object.chainType !== undefined && object.chainType !== null) {
+      message.chainType = object.chainType;
+    } else {
+      message.chainType = "";
+    }
+    if (object.sourcePort !== undefined && object.sourcePort !== null) {
+      message.sourcePort = object.sourcePort;
+    } else {
+      message.sourcePort = "";
+    }
+    if (object.sourceChannel !== undefined && object.sourceChannel !== null) {
+      message.sourceChannel = object.sourceChannel;
+    } else {
+      message.sourceChannel = "";
+    }
+    if (object.sender !== undefined && object.sender !== null) {
+      message.sender = object.sender;
+    } else {
+      message.sender = new Uint8Array();
+    }
+    if (object.toAddress !== undefined && object.toAddress !== null) {
+      message.toAddress = object.toAddress;
+    } else {
+      message.toAddress = new Uint8Array();
+    }
+    if (object.amount !== undefined && object.amount !== null) {
+      for (const e of object.amount) {
+        message.amount.push(Coin.fromPartial(e));
+      }
+    }
+    return message;
+  },
+};
+
+const baseMsgSendResponse: object = {};
+
+export const MsgSendResponse = {
+  encode(
+    _: MsgSendResponse,
+    writer: _m0.Writer = _m0.Writer.create()
+  ): _m0.Writer {
+    return writer;
+  },
+
+  decode(input: _m0.Reader | Uint8Array, length?: number): MsgSendResponse {
+    const reader = input instanceof _m0.Reader ? input : new _m0.Reader(input);
+    let end = length === undefined ? reader.len : reader.pos + length;
+    const message = { ...baseMsgSendResponse } as MsgSendResponse;
+    while (reader.pos < end) {
+      const tag = reader.uint32();
+      switch (tag >>> 3) {
+        default:
+          reader.skipType(tag & 7);
+          break;
+      }
+    }
+    return message;
+  },
+
+  fromJSON(_: any): MsgSendResponse {
+    const message = { ...baseMsgSendResponse } as MsgSendResponse;
+    return message;
+  },
+
+  toJSON(_: MsgSendResponse): unknown {
+    const obj: any = {};
+    return obj;
+  },
+
+  fromPartial(_: DeepPartial<MsgSendResponse>): MsgSendResponse {
+    const message = { ...baseMsgSendResponse } as MsgSendResponse;
+    return message;
+  },
+};
+
+export interface Msg {
+  Register(request: MsgRegisterAccount): Promise<MsgRegisterAccountResponse>;
+  Send(request: MsgSend): Promise<MsgSendResponse>;
+}
+
+export class MsgClientImpl implements Msg {
+  private readonly rpc: Rpc;
+  constructor(rpc: Rpc) {
+    this.rpc = rpc;
+  }
+  Register(request: MsgRegisterAccount): Promise<MsgRegisterAccountResponse> {
+    const data = MsgRegisterAccount.encode(request).finish();
+    const promise = this.rpc.request("intertx.Msg", "Register", data);
+    return promise.then((data) =>
+      MsgRegisterAccountResponse.decode(new _m0.Reader(data))
+    );
+  }
+
+  Send(request: MsgSend): Promise<MsgSendResponse> {
+    const data = MsgSend.encode(request).finish();
+    const promise = this.rpc.request("intertx.Msg", "Send", data);
+    return promise.then((data) => MsgSendResponse.decode(new _m0.Reader(data)));
+  }
+}
+
+interface Rpc {
+  request(
+    service: string,
+    method: string,
+    data: Uint8Array
+  ): Promise<Uint8Array>;
+}
+
+declare var self: any | undefined;
+declare var window: any | undefined;
+var globalThis: any = (() => {
+  if (typeof globalThis !== "undefined") return globalThis;
+  if (typeof self !== "undefined") return self;
+  if (typeof window !== "undefined") return window;
+  if (typeof global !== "undefined") return global;
+  throw "Unable to locate global object";
+})();
+
+const atob: (b64: string) => string =
+  globalThis.atob ||
+  ((b64) => globalThis.Buffer.from(b64, "base64").toString("binary"));
+function bytesFromBase64(b64: string): Uint8Array {
+  const bin = atob(b64);
+  const arr = new Uint8Array(bin.length);
+  for (let i = 0; i < bin.length; ++i) {
+    arr[i] = bin.charCodeAt(i);
+  }
+  return arr;
+}
+
+const btoa: (bin: string) => string =
+  globalThis.btoa ||
+  ((bin) => globalThis.Buffer.from(bin, "binary").toString("base64"));
+function base64FromBytes(arr: Uint8Array): string {
+  const bin: string[] = [];
+  for (let i = 0; i < arr.byteLength; ++i) {
+    bin.push(String.fromCharCode(arr[i]));
+  }
+  return btoa(bin.join(""));
+}
+
+type Builtin =
+  | Date
+  | Function
+  | Uint8Array
+  | string
+  | number
+  | boolean
+  | undefined
+  | Long;
+export type DeepPartial<T> = T extends Builtin
+  ? T
+  : T extends Array<infer U>
+  ? Array<DeepPartial<U>>
+  : T extends ReadonlyArray<infer U>
+  ? ReadonlyArray<DeepPartial<U>>
+  : T extends {}
+  ? { [K in keyof T]?: DeepPartial<T[K]> }
+  : Partial<T>;
+
+if (_m0.util.Long !== Long) {
+  _m0.util.Long = Long as any;
+  _m0.configure();
+}
