f107e8ea946614fec98849be1396c00f3c1a32db
Ryan Gordon <ryangordon210@gmail.com>
2021-05-08 11:53:40 +0100

    feat(sendTx): Initial SendTX for Bank and Intertx

--- src/App.tsx
+++ src/App.tsx
@@ -5,47 +5,39 @@ import './App.css';
 import { DirectSecp256k1HdWallet, Registry, GeneratedType } from "@cosmjs/proto-signing";
 import { createProtobufRpcClient, QueryClient } from "@cosmjs/stargate";
 import { toAccAddress } from "@cosmjs/stargate/build/queries/utils"
-import { defaultRegistryTypes, SigningStargateClient } from "@cosmjs/stargate";
+import { defaultRegistryTypes, SigningStargateClient, coins } from "@cosmjs/stargate";
 import { setupBankExtension } from "@cosmjs/stargate/build/queries/bank"
 import { Bech32 } from "@cosmjs/encoding"
-import { MsgRegisterAccount } from "./codec/intertx/tx";
+import { MsgRegisterAccount, MsgSend as InterTxMsgSend } from "./codec/intertx/tx";
 import { QueryClientImpl } from "./codec/intertx/query";
 import { Tendermint34Client } from "@cosmjs/tendermint-rpc";
-const myTypes: ReadonlyArray<[string, GeneratedType]> = [["/intertx.MsgRegisterAccount", MsgRegisterAccount]]
+const myTypes: ReadonlyArray<[string, GeneratedType]> = [["/intertx.MsgRegisterAccount", MsgRegisterAccount],["/intertx.MsgSend", InterTxMsgSend]]
 
 const myRegistry = new Registry([
   ...defaultRegistryTypes,
   ...myTypes,
 ]);
 
-const mnemonic = // Replace with your own mnemonic
+const def_mnemonic = // Replace with your own mnemonic
   "alley afraid soup fall idea toss can goose become valve initial strong forward bright dish figure check leopard decide warfare hub unusual join cart";
 
+const MNEMONIC_2="record gift you once hip style during joke field prize dust unique length more pencil transfer quit train device arrive energy sort steak upset"
+
 const DEFAULT_ENDPOINT = "localhost:16657"
+const SECOND_ENDPOINT = "localhost:26657"
 const COSM_DEV_ADDRESS_1 = "cosmos1mjk79fjjgpplak5wq838w0yd982gzkyfrk07am";
+const COSM_DEV_ADDRESS_2 = "cosmos17dtl0mjt3t77kpuhg2edqzjpszulwhgzuj9ljs";
+const IBC_ACCOUNT = "cosmos1pt6ar8lawmvvq5haxc3l3zhjfl04u56fs2ndh9"
 
 
-async function getClientforWalletMnemonic() {
-  // TODO: Remove after documenting the difference between approaches
-  // const signer = await DirectSecp256k1HdWallet.fromMnemonic(
-  //   mnemonic,
-  //   makeCosmoshubPath(0),
-  //   "cosmos1", // Replace with your own Bech32 address prefix
-  // );
-  // const client = await SigningStargateClient.connectWithSigner(
-  //   DEFAULT_ENDPOINT, // Replace with your own RPC endpoint
-  //   signer,
-  //   {
-  //     registry: myRegistry,
-  //   },
-  // );
+async function getClientforWalletMnemonic(endpoint: string = DEFAULT_ENDPOINT, mnemonic: string = def_mnemonic) {
   const wallet = await DirectSecp256k1HdWallet.fromMnemonic(mnemonic);
-  const client = await SigningStargateClient.connectWithSigner(DEFAULT_ENDPOINT, wallet, {
+  const client = await SigningStargateClient.connectWithSigner(endpoint, wallet, {
     registry: myRegistry,
   });
   return client
 }
-async function makeBroadcastTx() {
+async function makeRegisterTx() {
 
   const client = await getClientforWalletMnemonic();
   const myAddress = COSM_DEV_ADDRESS_1;
@@ -70,12 +62,11 @@ async function makeBroadcastTx() {
   // Inside an async function...
   // This method uses the registry you provided
   const response = await client.signAndBroadcast(myAddress, [message], fee);
-  console.log("Finished register Request")
+  console.log("Finished makeBroadcastTx Request")
 
 }
 
-async function makeAddressQueryTx() {
-  const queryAddress = COSM_DEV_ADDRESS_1
+async function makeAddressQueryTx(queryAddress:string) {
   // The Tendermint client knows how to talk to the Tendermint RPC endpoint
   const tendermintClient = await Tendermint34Client.connect(DEFAULT_ENDPOINT);
 
@@ -94,13 +85,13 @@ async function makeAddressQueryTx() {
     port: "ibcaccount",
     channel: "channel-0"
   });
-  console.log("Finished Request")
+  console.log("Finished makeAddressQueryTx Request")
   console.log(Bech32.encode("cosmos", queryResult.address))
 }
 
 async function makeBankQueryTx(queryAddress: string) {
   // The Tendermint client knows how to talk to the Tendermint RPC endpoint
-  const tendermintClient = await Tendermint34Client.connect(DEFAULT_ENDPOINT);
+  const tendermintClient = await Tendermint34Client.connect(SECOND_ENDPOINT);
 
   // The generic Stargate query client knows how to use the Tendermint client to submit unverified ABCI queries
   // const queryClient = new QueryClient(tendermintClient);
@@ -109,10 +100,6 @@ async function makeBankQueryTx(queryAddress: string) {
     setupBankExtension,
     // You can add up to 8 extensions
   );
-  
-  // // Inside an async function...
-  // // Now your query client has been extended
-  // const queryResult = await queryClient.my.nested.customQuery("bar");
 
   // This helper function wraps the generic Stargate query client for use by the specific generated query client
   const rpcClient = createProtobufRpcClient(queryClient);
@@ -123,16 +110,71 @@ async function makeBankQueryTx(queryAddress: string) {
 
   // Now you can use this service to submit queries
   const queryResult = await queryClient.bank.balance(queryAddress, "stake")
-  console.log("Finished Request")
+  console.log("Finished makeBankQueryTx Request")
   console.log(`Account ${queryAddress} has a balance of ${queryResult.amount} ${queryResult.denom}`)
+}
+
+async function makeBankSendTx(sendAddress: string, receivingAddress:string, amount:number, denom:string, chain:string){
+  const client = await getClientforWalletMnemonic(chain, MNEMONIC_2);
+  
+  const transferAmount = coins(amount, denom)
+  // const signedTX = await client.sendIbcTokens(sendAddress, receivingAddress, transferAmount)
+  const response = await client.sendTokens(sendAddress, receivingAddress, transferAmount);
+  console.log("Finished bank send Request")
+  console.log(response)
+}
+
+async function makeIntertxSendTx(sendAddress:string, receivingAddress:string, amount:number, denom:string, gas:number = 90000){
+  const client = await getClientforWalletMnemonic();
+  const transferAmount = coins(amount, denom)
+  const message = {
+    typeUrl: "/intertx.MsgSend", // Same as above
+    value: {
+      sender: toAccAddress(sendAddress),
+      toAddress: toAccAddress(receivingAddress),
+      sourcePort: "ibcaccount",
+      sourceChannel: "channel-0",
+      chainId: "test-1",
+      amount: transferAmount
+    },
+  };
+  const fee = {
+    amount: [
+      {
+        denom: denom, // Use the appropriate fee denom for your chain
+        amount: "90000",
+      },
+    ],
+    gas: "90000",
+  };
+
+  //TODO: Review just using 
+  // const response = await client.sendIbcTokens(sendAddress, receivingAddress, transferAmount, "ibcaccount", "channel-0");
+
+  const response = await client.signAndBroadcast(sendAddress, [message], fee);
+  console.log("Finished intertx send Request")
+  console.log(response)
+  
 }
 
 class App extends Component {
 
+  async componentDidMount(){
+    await makeRegisterTx()
+    await makeAddressQueryTx(COSM_DEV_ADDRESS_1)
+    await makeBankQueryTx(IBC_ACCOUNT)
+    await makeBankSendTx(COSM_DEV_ADDRESS_2, IBC_ACCOUNT, 1000, "stake", SECOND_ENDPOINT)
+    await makeBankQueryTx(IBC_ACCOUNT)
+    await makeIntertxSendTx(COSM_DEV_ADDRESS_1, COSM_DEV_ADDRESS_2, 500, "stake", 90000)
+    await makeBankQueryTx(IBC_ACCOUNT)
+    await makeBankQueryTx(COSM_DEV_ADDRESS_1)
+    await makeBankQueryTx(COSM_DEV_ADDRESS_2)
+  }
+
   render() {
-    makeBroadcastTx()
-    makeAddressQueryTx()
-    makeBankQueryTx("cosmos1pt6ar8lawmvvq5haxc3l3zhjfl04u56fs2ndh9")
+    
+
+
     return (
       <div className="App">
         <header className="App-header">
